73765355fefcdd22549478d9bfd8ce93dad10b72
octavioricci <octavioricci@gmail.com>
2017-06-02 14:52:20 -0300

    Add files via upload

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,44 @@
+﻿using Simple.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Simple.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        [HttpPost]
+        public JsonResult AjaxMethod(string name)
+        {
+
+            PersonaModel persona = new PersonaModel
+            {
+                Nombre = name,
+                DateTime = DateTime.Now.ToString()
+            };
+
+            return Json(persona);
+        }
+    }
+}
