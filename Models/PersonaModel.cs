e08be319832a1e6c0f22900e395710ecb721251e
octavioricci <octavioricci@gmail.com>
2017-06-02 14:52:34 -0300

    Add files via upload

---
+++ Models/PersonaModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Simple.Models
+{
+    public class PersonaModel
+    {
+        public string Nombre { get; set; }
+        public string DateTime { get; set; }
+    }
+}
